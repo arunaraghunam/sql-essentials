17270789240d83f681f4ff99578c17b3ba8c8640
arunaraghunam <94058831+arunaraghunam@users.noreply.github.com>
2023-04-21 07:08:57 +0100

    Add files via upload
    DATETRUNC - Truncates a date, time or timestamp to a specifed position - available in SQL Server 2022

---
+++ DATETRUNC date function.sql
@@ -0,0 +1,14 @@
+DECLARE @d datetime2 = '2023-04-21 07:03:15.1234567';
+
+SELECT 'Year', DATETRUNC(year, @d);
+SELECT 'Quarter', DATETRUNC(quarter, @d);
+SELECT 'Month', DATETRUNC(month, @d);
+SELECT 'Week', DATETRUNC(week, @d); -- Using the default DATEFIRST setting value of 7 (U.S. English)
+SELECT 'Iso_week', DATETRUNC(iso_week, @d);
+SELECT 'DayOfYear', DATETRUNC(dayofyear, @d);
+SELECT 'Day', DATETRUNC(day, @d);
+SELECT 'Hour', DATETRUNC(hour, @d);
+SELECT 'Minute', DATETRUNC(minute, @d);
+SELECT 'Second', DATETRUNC(second, @d);
+SELECT 'Millisecond', DATETRUNC(millisecond, @d);
+SELECT 'Microsecond', DATETRUNC(microsecond, @d);
