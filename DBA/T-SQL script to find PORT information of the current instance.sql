1f5718eecf95e8789aa5594b781c619ee5b38875
arunaraghunam <94058831+arunaraghunam@users.noreply.github.com>
2023-04-25 13:11:09 +0100

    Add files via upload

---
+++ DBA/T-SQL script to find PORT information of the current instance.sql
@@ -0,0 +1,21 @@
+--Below T-SQL script to find PORT information of the current instance:
+
+SET NOCOUNT ON
+DECLARE 
+	@Port VARCHAR(30)
+	,@Key VARCHAR(150)
+IF CHARINDEX('\',@@SERVERNAME,0) <>0
+BEGIN 
+	SET @Key = 'SOFTWARE\MICROSOFT\Microsoft SQL Server\'
+				+@@SERVICENAME+'\MSSQLServer\Supersocketnetlib\TCP'
+END
+ELSE
+BEGIN 
+	SET @Key = 'SOFTWARE\MICROSOFT\MSSQLServer\MSSQLServer \Supersocketnetlib\TCP'
+END 
+EXEC master.sys.xp_regread 
+	@Rootkey='HKEY_LOCAL_MACHINE'
+	,@Key=@Key,@value_name='Tcpport'
+	,@value=@Port OUTPUT
+ 
+SELECT 'SQL Server Name: '+@@SERVERNAME + ' Port # '+CONVERT(VARCHAR(20),@Port)
